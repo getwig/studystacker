573ab23de9282f21b6712e179aa2381bbb26ed29
getwig <lootwig@icloud.com>
2025-09-26 00:48:37 +0200

    Add @vercel/speed-insights integration and update layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,24 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "sonner";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { Toaster } from 'sonner';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Studystacker",
-  description: "Behalte deine Schulbücher im Überblick",
+  title: 'Studystacker',
+  description: 'Behalte deine Schulbücher im Überblick',
   icons: {
-    icon: "/images/logo/icon.svg",
+    icon: '/images/logo/icon.svg',
   },
 };
 
@@ -27,12 +28,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="de">
+    <html lang='de'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
         {children}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
